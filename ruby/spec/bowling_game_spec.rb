605566bc9be0784568492fa14d5d63f7fd8006b4
Michel Grootjans <michel.grootjans@gmail.com>
2015-09-23 21:04:38 +0200

    extracted the bowling game

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -1,26 +1,43 @@
-describe "BowlingGame" do
-  def score
-    0
-  end
+require 'bowling_game'
 
-  def roll pins
-  end
+describe "BowlingGame: " do
+  let(:game){ BowlingGame.new }
 
-  it "scores 0" do
-    expect(score).to eq 0
-  end
-
-  it "rolling a 0 scores 0" do
-    roll 0
-    expect(score).to eq 0
-  end
-
-  it "rolling a gutter game scores 0" do
-    10.times do 
-      roll 0
-      roll 0
+  context 'a new game' do
+    it "scores 0" do
+      expect(game.score).to eq 0
     end
+  end
 
-    expect(score).to eq 0
+  context "rolling a 0" do
+    before { game.roll 0 }
+    it 'scores 0' do
+      expect(game.score).to eq 0
+    end
+  end
+
+  context "rolling a gutter game" do
+    before { 20.times {game.roll 0} }
+    it 'scores 0' do
+      expect(game.score).to eq 0
+    end
+  end
+
+  context "rolling a 1" do
+    before { game.roll 1 }
+    it do
+      expect(game.score).to eq 1
+    end
+  end
+
+  it "rolling 1 twice scores 2" do
+    game.roll 1
+    game.roll 1
+    expect(game.score).to eq 2
+  end
+
+  it "rolling 2 scores 2" do
+    game.roll 2
+    expect(game.score).to eq 2
   end
 end
