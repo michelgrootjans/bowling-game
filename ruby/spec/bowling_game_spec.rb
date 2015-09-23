926e1d1ace8b797c5373256977008b43d22fc259
Michel Grootjans <michel.grootjans@gmail.com>
2015-09-23 21:05:09 +0200

    introduced a matcher

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -1,43 +1,40 @@
 require 'bowling_game'
 
+RSpec::Matchers.define :score do |expected|
+  match do |game|
+    game.score == expected
+  end
+end
+
 describe "BowlingGame: " do
   let(:game){ BowlingGame.new }
 
   context 'a new game' do
-    it "scores 0" do
-      expect(game.score).to eq 0
-    end
+    it { expect(game).to score 0 }
   end
 
   context "rolling a 0" do
     before { game.roll 0 }
-    it 'scores 0' do
-      expect(game.score).to eq 0
-    end
+    it { expect(game).to score 0 }
   end
 
   context "rolling a gutter game" do
     before { 20.times {game.roll 0} }
-    it 'scores 0' do
-      expect(game.score).to eq 0
-    end
+    it { expect(game).to score 0 }
   end
 
   context "rolling a 1" do
     before { game.roll 1 }
-    it do
-      expect(game.score).to eq 1
-    end
+    it { expect(game).to score 1 }
   end
 
-  it "rolling 1 twice scores 2" do
-    game.roll 1
-    game.roll 1
-    expect(game.score).to eq 2
+  context "rolling 1 twice scores 2" do
+    before { 2.times { game.roll 1 } }
+    it { expect(game).to score 2 }
   end
 
-  it "rolling 2 scores 2" do
-    game.roll 2
-    expect(game.score).to eq 2
+  context "rolling a 2" do
+    before { game.roll 2 }
+    it { expect(game).to score 2 }
   end
 end
