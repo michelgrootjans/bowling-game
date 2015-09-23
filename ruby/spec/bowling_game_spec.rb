88d0cdbc908de81fb6a2d4314e480f4e813bd12f
Michel Grootjans <michel.grootjans@gmail.com>
2015-09-23 20:38:15 +0200

    still very basic tests

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -1,6 +1,26 @@
 describe "BowlingGame" do
+  def score
+    0
+  end
+
+  def roll pins
+  end
+
   it "scores 0" do
-    score = 0
+    expect(score).to eq 0
+  end
+
+  it "rolling a 0 scores 0" do
+    roll 0
+    expect(score).to eq 0
+  end
+
+  it "rolling a gutter game scores 0" do
+    10.times do 
+      roll 0
+      roll 0
+    end
+
     expect(score).to eq 0
   end
 end
